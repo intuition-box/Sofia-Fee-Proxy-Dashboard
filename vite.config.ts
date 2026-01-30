7e4fc189704346c0f600426f0bab7274c4eb8f90
Zet <socials.jeremie.olivier@gmail.com>
2026-01-30 17:40:29 +0100

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Sofia-Fee-Proxy-Dashboard/',
+  base: '/',
   plugins: [react(), tailwindcss()],
 })
