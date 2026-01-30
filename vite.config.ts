78f6dda2c9782c4c81d0310cd04f22a6cb2b415d
Wieedze <maxime.moodz@gmail.com>
2026-01-30 17:16:57 +0100

    workflow setup

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Sofia-Fee-Proxy-Dashboard/',
   plugins: [react(), tailwindcss()],
 })
